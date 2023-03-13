ff373a22cd7e441740f47e0e78809a8b8960799a
ssb-jnk <JohnnyNKarlsen@hotmail.com>
2023-03-13 13:21:16 +0100

    Various fixes (#9)
    * STATBANK_BASE_URL should default to i.test.ssb.no
    
    * added symlink from /ssb/share/etc/stamme_variabel to /etc/profile.d/stamme_variabel
    
    * removed file stamme_variabel
    
    * added exports in start.sh so they are available in rstudio-session

--- docker/rstudio/start.sh
+++ docker/rstudio/start.sh
@@ -5,6 +5,9 @@ echo "Running custom startup script"
 # sourcing stamme_variabel in startup they are available to all users in RStudio
 source /etc/profile.d/stamme_variabel
 
+export STATBANK_BASE_URL=$STATBANK_BASE_URL
+export STATBANK_ENCRYPT_URL=$STATBANK_ENCRYPT_URL
+
 # updating CA-certificate before starting RStudio so users can access github
 update-ca-certificates
 
