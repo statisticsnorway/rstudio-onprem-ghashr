22f194cc6f8d4963e85efc4c80705f9f7ab7a9f0
ssbsjv <156660008+ssbsjv@users.noreply.github.com>
2024-09-30 09:12:00 +0200

    Testing addition of RJDemetra to patch 6 (#54)
    * Update r-packages-src.R
    
    Remoivng RJDemetra, and changing location of sdctable and easysdctable to .txt-file
    
    * Update r-packages.txt
    
    adding sdc and easysdctable
    
    * Update r-packages.txt
    
    removed sdctable etc
    
    * Reordering packages
    
    * Update r-packages-git.R
    
    * Update Dockerfile
    
    * Update r-packages-src.R

--- docker/rstudio/r-packages-src.R
+++ docker/rstudio/r-packages-src.R
@@ -1,6 +1,6 @@
 pkgs <- c(
-    'DBI','leaflet','getPass', 'rJava','rjwsacruncher','sfarrow','Rcurl', 'esquisse','dcmodify','googleCloudStorageR','tidyfst', 'Hmisc','sdcTable', 'DT', 'configr','dggridR',
-    'renv', 'openxlsx', 'survey', 'git2r', 'RJDemetra', 'eurostat', 'sdcTable', 'simputation',
+    'DBI','leaflet','getPass', 'rJava','rjwsacruncher','sfarrow','Rcurl', 'esquisse','dcmodify','googleCloudStorageR','tidyfst', 'Hmisc', 'DT', 'configr','dggridR',
+    'renv', 'openxlsx', 'survey', 'git2r', 'eurostat', 'simputation','sdcTable','RJDemetra',
     'SmallCountRounding',
     'PxWebApiData',
     'SSBtools',
@@ -11,6 +11,14 @@ pkgs <- c(
 install.packages(pkgs, dependencies=TRUE, repos='https://packagemanager.rstudio.com/cran/latest')
 
 install.packages('/tmp/ROracle_1.4-1_R_x86_64-unknown-linux-gnu.tar.gz', repos = NULL, type='source')
+
+remotes::install_github('statisticsnorway/ssb-pris')
+remotes::install_github('statisticsnorway/ssb-kostra')
+remotes::install_github('statisticsnorway/ssb-sdcforetakperson')
+remotes::install_github('statisticsnorway/ssb-struktur')
+remotes::install_github('statisticsnorway/ssb-fellesr', dependencies=TRUE, verbose=TRUE)
+remotes::install_github('statisticsnorway/ssb-easysdctable')
+remotes::install_github('statisticsnorway/ReGenesees')
 
 install.packages('arrow', dependencies=FALSE, repos='https://packagemanager.rstudio.com/cran/latest')
 
