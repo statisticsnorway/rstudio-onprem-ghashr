33357e181e0565947d1a341b2c032ea15f8805fb
ssbsjv <156660008+ssbsjv@users.noreply.github.com>
2024-10-14 13:45:53 +0200

    Ssbsjv patch 19 (#70)
    * Clean-up
    
    * Update r-packages-src.R
    
    * Update r-packages-src.R
    
    * Update Dockerfile

--- docker/rstudio/r-packages-src.R
+++ docker/rstudio/r-packages-src.R
@@ -1,13 +1,22 @@
 pkgs <- c(
-    'DBI','leaflet','getPass', 'rJava','rjwsacruncher','sfarrow','Rcurl', 'esquisse','dcmodify','googleCloudStorageR','tidyfst', 'Hmisc', 'DT', 'configr','dggridR',
-    'renv', 'openxlsx', 'survey', 'git2r', 'eurostat', 'simputation','sdcTable','RJDemetra',
+    'RJDemetra',
     'SmallCountRounding',
     'PxWebApiData',
+    'openxlsx',
     'SSBtools',
-    'klassR',
     'GISSB',
-    'GaussSuppression'
-)
+    'GaussSuppression', 
+    'tinytest', 
+    'configr', 
+    'DT', 
+    'dcmodify', 
+    'simputation', 
+    'survey', 
+    'srvyr', 
+    'eurostat', 
+    'dggridR', 
+    'tidyfst')
+
 install.packages(pkgs, dependencies=TRUE, repos='https://packagemanager.rstudio.com/cran/latest')
 
 install.packages('/tmp/ROracle_1.4-1_R_x86_64-unknown-linux-gnu.tar.gz', repos = NULL, type='source')
@@ -20,5 +29,5 @@ remotes::install_github('statisticsnorway/ssb-fellesr', dependencies=TRUE, verbo
 remotes::install_github('statisticsnorway/ssb-easysdctable', dependencies=TRUE, verbose=TRUE)
 remotes::install_github('statisticsnorway/ReGenesees')
 
-install.packages('arrow', dependencies=FALSE, repos='https://packagemanager.rstudio.com/cran/latest')
+#install.packages('arrow', dependencies=FALSE, repos='https://packagemanager.rstudio.com/cran/latest')
 
